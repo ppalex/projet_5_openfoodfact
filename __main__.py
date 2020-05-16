e70cafc1b928d3b6924a6d76ef7c6863b5ba8413
ppalex <prietopantoja.alex@gmail.com>
2020-05-16 14:02:32 +0200

    gitignore and main3 3

--- __main__.py
+++ __main__.py
@@ -1,4 +1,5 @@
 import argparse
+import pdb
 
 from initialize import initialize_job
 
@@ -7,16 +8,18 @@ def main():
     pass
 
 
-if __name__ == "main":
+if __name__ == "__main__":
+    
     parser = argparse.ArgumentParser(description='OpenFoodFact')
 
     parser.add_argument('--job', type=str, nargs=1,
                         help='Launch the script to create tables in DB, \
                                 get data from API and fill DB with data.')
     args = parser.parse_args()
-
+    
     if args.job:
         if args.job[0] == 'initialize':
+            
             initialize_job()
 
         elif args.job[0] == 'drop_tables':
