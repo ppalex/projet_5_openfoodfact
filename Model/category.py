3f749176bbaecc90a490f33731bc90ccc6f01a82
ppalex <prietopantoja.alex@gmail.com>
2020-05-16 09:30:57 +0200

    modif

--- Model/category.py
+++ Model/category.py
@@ -2,5 +2,5 @@ class Category:
     def __init__(self, category_name):
         self.category_name = category_name
 
-    def __repr__(self):
+    def __str__(self):
         return f"{self.category_name}"
