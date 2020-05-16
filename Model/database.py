47ee7d5ed849e2d1e9fbda980c0db2ef418440d0
ppalex <prietopantoja.alex@gmail.com>
2020-05-16 09:16:04 +0200

    modification database.py

--- Model/database.py
+++ Model/database.py
@@ -1,14 +1,17 @@
 import mysql.connector
+import Configuration.config as config
+
+config.load('./configuration/config.yml')
 
 class Database:
     _instance = None
     def __new__(self):
         if not self._instance:
             self._instance  = super(Database, self).__new__(self)
-            self.db = mysql.connector.connect(host='localhost',
-                                              user='root',
-                                              password='Ingestic13&',
-                                              database='openfoodfact',
+            self.db = mysql.connector.connect(host=config.value['DB']['host'],
+                                              user=config.value['DB']['user'],
+                                              password=config.value['DB']['password'],
+                                              database=config.value['DB']['database'],
                                               auth_plugin='mysql_native_password')
 
         return self._instance
